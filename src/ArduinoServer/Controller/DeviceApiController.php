feb31a0e73b2b63fa7f4e08866c8472a24201f2f
milos92100 <milos92100@gmail.com>
2023-04-09 18:31:34 +0200

    read query url

--- src/ArduinoServer/Controller/DeviceApiController.php
+++ src/ArduinoServer/Controller/DeviceApiController.php
@@ -6,12 +6,15 @@ namespace ArduinoServer\Controller;
 
 use ArduinoServer\Core\Http\HttpResponse;
 use ArduinoServer\Core\Controller;
+use ArduinoServer\Core\Http\Abstraction\HttpRequestInterface;
 
 class DeviceApiController extends Controller {
 
-    public function sync(): HttpResponse {
+    public function sync(HttpRequestInterface $request): HttpResponse {
+        error_log($request->getQuery("test"));
+        
         $response = new HttpResponse();
-        $response->setContent("Cao Jovana");
+        $response->setContent(var_dump($_GET));
         return $response;
     }
 
